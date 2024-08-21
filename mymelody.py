1ae1290f83d578a4360924094d8fd7ec84fea366
rpcauk <rpcauk@protonmail.com>
2024-08-22 00:47:41 +0100

    Update to reduce api calls

--- mymelody.py
+++ mymelody.py
@@ -58,6 +58,14 @@ class MyMelody:
     def get_content_metadata(cls, content_type, content_id, args={}):
         return getattr(cls.CLIENT, content_type)(content_id, **args)
 
+    # @classmethod
+    # def get_track(cls, track_id):
+    #     return cls.CLIENT.track(track_id)
+
+    # @classmethod
+    # def get_album_tracks(cls, album, limit=50, offset=0):
+    #     return
+
     # Config
     @classmethod
     def load_config(cls, path="config.json"):
@@ -111,7 +119,7 @@ def sanitize_name(name):
 
 def antiban_wait(seconds=5):
     for i in range(seconds)[::-1]:
-        print(f"\rSleep for {i + 1} second(s)...", end="")
+        print(f"\r  Sleep for {i + 1} second(s)...", end="")
         time.sleep(1)
     print()
 
@@ -120,60 +128,71 @@ def antiban_wait(seconds=5):
 # Metadata                                                                     #
 ################################################################################
 
-def get_artist_metadata(artist_id):
-    existing_data = MyMelody.get_data("artists", artist_id)
-    if existing_data:
-        return existing_data
+def set_artist_metadata(artist, update=True):
+    # existing_data = MyMelody.get_data("artists", artist["id"])
+    # if existing_data:
+    #     return existing_data
 
-    resp = MyMelody.get_content_metadata("artist", artist_id)
-    artist_data = {}
-    artist_data["name"] = resp["name"]
-    artist_data["artwork_url"] = sorted(resp["images"], key=lambda i: i["height"], reverse=True)[0]["url"]
-    artist_data["follow"] = True
+    # resp = MyMelody.get_content_metadata("artist", artist_id)
+    data = {}
+    data["name"] = artist["name"]
+    data["artwork_url"] = sorted(artist["images"], key=lambda i: i["height"], reverse=True)[0]["url"]
+    data["follow"] = True
 
-    MyMelody.update_data("artists", artist_id, artist_data)
-    return artist_data
+    if update:
+        MyMelody.update_data("artists", artist["id"], data)
+    return data
 
 
-def get_track_metadata(track_id):
-    existing_data = MyMelody.get_data("tracks", track_id)
-    if existing_data:
-        return existing_data
+def set_track_metadata(track, update=True):
+    # existing_data = MyMelody.get_data("tracks", track_id)
+    # if existing_data:
+    #     return existing_data
 
-    resp = MyMelody.get_content_metadata("track", track_id)
+    # track_data = MyMelody.get_content_metadata("track", track_id)
     track_data = {}
-    track_data["name"] = resp["name"]
+    track_data["name"] = track["name"]
 
-    match resp["album"]["release_date_precision"]:
+    match track["album"]["release_date_precision"]:
         case "day":
-            track_data["release_date"] = resp["album"]["release_date"]
+            track_data["release_date"] = track["album"]["release_date"]
         case "month":
-            track_data["release_date"] = resp["album"]["release_date"] + "-01"
+            track_data["release_date"] = track["album"]["release_date"] + "-01"
         case "year":
-            track_data["release_date"] = resp["album"]["release_date"] + "-01-01"
+            track_data["release_date"] = track["album"]["release_date"] + "-01-01"
 
-    track_data["disc_number"] = str(resp["disc_number"])
-    track_data["track_number"] = str(resp["track_number"])
+    track_data["disc_number"] = str(track["disc_number"])
+    track_data["track_number"] = str(track["track_number"])
     track_data["album"] = {
-        "id": resp["album"]["id"],
-        "name": resp["album"]["name"],
-        "artists": {x["id"]: {k:v for k,v in x.items() if k in ("name")} for x in resp["album"]["artists"]}
+        "id": track["album"]["id"],
+        "name": track["album"]["name"],
+        "artists": {x["id"]: {k:v for k,v in x.items() if k in ("name")} for x in track["album"]["artists"]},
+        "album_type": track["album"]["album_type"]
     }
-    track_data["artists"] = {x["id"]: {k:v for k,v in x.items() if k in ("name")} for x in resp["artists"]}
-    track_data["artwork_url"] = sorted(resp["album"]["images"], key=lambda i: i["height"], reverse=True)[0]["url"]
+    track_data["artists"] = {x["id"]: {k:v for k,v in x.items() if k in ("name")} for x in track["artists"]}
+    track_data["artwork_url"] = sorted(track["album"]["images"], key=lambda i: i["height"], reverse=True)[0]["url"]
     track_data["ignore"] = False
 
-    MyMelody.update_data("tracks", track_id, track_data)
+    if update:
+        MyMelody.update_data("tracks", track["id"], track_data)
     return track_data
 
 
-def get_track_description(track_id):
-    track_data = get_track_metadata(track_id)
-    return track_data["name"] + " - " + "; ".join([x["name"] for x in track_data["artists"].values()])
+def get_track_description(track_id, album=False, artists=True):
+    track_data = MyMelody.get_data("tracks", track_id)
+    track_name = track_data["name"]
+    track_album = track_data["album"]["name"]
+    track_artists = "; ".join([x["name"] for x in track_data["artists"].values()])
+    track_string = track_name
+    if album:
+        track_string += " - " + track_album
+    if artists:
+        track_string += " - " + track_artists
+    return track_string
 
 
 def get_track_path(track_id):
-    track_data = get_track_metadata(track_id)
+    track_data = MyMelody.get_data("tracks", track_id)
     track_path = MyMelody.get_track_path()
     for k,v in track_data['album']['artists'].items():
         track_path += f"/{sanitize_name(v['name'])} [{k}]"
@@ -186,8 +205,17 @@ def get_track_path(track_id):
 # Download                                                                     #
 ################################################################################
 
+def tracks_to_download():
+    track_ids = []
+    for track_id, track_data in MyMelody.get_data("tracks").items():
+        track_path = get_track_path(track_id)
+        if track_data["ignore"] or os.path.exists(track_path):
+            continue
+        track_ids.append(track_id)
+    return track_ids
+
 def set_track_tags(track_id):
-    track_data = get_track_metadata(track_id)
+    track_data = MyMelody.get_data("tracks", track_id)
     track_path = get_track_path(track_id)
 
     track = MP3(track_path, ID3=EasyID3)
@@ -211,18 +239,18 @@ def set_track_tags(track_id):
 
 
 def download_track(track_id):
-    track_data = get_track_metadata(track_id)
+    track_data = MyMelody.get_data("tracks", track_id)
     track_path = get_track_path(track_id)
 
     if track_data["ignore"] or os.path.exists(track_path):
-        print(f"Skipping {get_track_description(track_id)}")
+        print(f"  Skipping {get_track_description(track_id)}")
         return False
 
     pathlib.Path(os.path.dirname(track_path)).mkdir(parents=True, exist_ok=True)
     with tempfile.NamedTemporaryFile() as fh:
         stream = MyMelody.get_content_stream(TrackId.from_uri(f"spotify:track:{track_id}"))
         total_size = stream.input_stream.size
-        progress = tqdm(total=total_size, desc=get_track_description(track_id))
+        progress = tqdm(total=total_size, desc="  "+get_track_description(track_id))
         downloaded = 0
         fail_count = 0
         while downloaded < total_size:
@@ -253,11 +281,17 @@ def download_track(track_id):
 
 
 def download_tracks_safely(track_ids):
-    num_tracks = 5
     downloaded = 0
     for i in range(len(track_ids)):
-        if downloaded != 0 and downloaded % num_tracks == 0:
-            antiban_wait()
+        if downloaded != 0:
+            if downloaded % 100 == 0:
+                antiban_wait(seconds=60)
+            elif downloaded % 50 == 0:
+                antiban_wait(seconds=30)
+            elif downloaded % 25 == 0:
+                antiban_wait(seconds=10)
+            elif downloaded % 5 == 0:
+                antiban_wait(seconds=5)
         download_resp = download_track(track_ids[i])
         if download_resp:
             downloaded += 1
@@ -272,8 +306,7 @@ def cleanup_tracks():
             if os.path.exists(track_path):
                 os.remove(track_path)
                 print("  " + track_path)
-    else:
-        print("No tracks to delete")
+        print()
 
     deleted = set()
     for current_dir, subdirs, files in os.walk(MyMelody.get_track_path(), topdown=False):
@@ -291,8 +324,7 @@ def cleanup_tracks():
         for directory in deleted:
             os.rmdir(directory)
             print("  " + directory)
-    else:
-        print("No directories to delete")
+        print()
 
 
 ################################################################################
@@ -300,44 +332,122 @@ def cleanup_tracks():
 ################################################################################
 
 def process_tracks(track_ids):
-    progress = tqdm(total=len(track_ids))
-    for track_id in track_ids:
-        progress.set_description(get_track_description(track_id))
-        progress.update(1)
-    progress.close()
-    return track_ids
+    tracks = {}
+    chunk_size = 50
+    chunks = [track_ids[i:i+chunk_size] for i in range(0,len(track_ids),chunk_size)]
+
+    # progress = tqdm(total=len(track_ids)) # , disable=(not update)
+    for chunk in chunks:
+        # track_data = set_track_metadata(MyMelody.CLIENT.track(track_id))
+        # tracks[track_id] = track_data
+        # progress.set_description(get_track_description(track_id))
+        # progress.update(1)
+        tracks = MyMelody.CLIENT.tracks(chunk)
+        for track in tracks["tracks"]:
+            tracks[track["id"]] = set_track_metadata(track)
+            print("  " + get_track_description(track["id"]))
+    # progress.close()
+    return tracks
 
 
 def process_albums(album_ids):
-    track_ids = []
-    for album_id in album_ids:
-        album_data = MyMelody.get_content_metadata("album", album_id)
-        print(album_data["name"] + " - " + "; ".join([x["name"] for x in album_data["artists"]]))
-        track_ids += process_tracks([x["id"] for x in album_data["tracks"]["items"]])
-    return track_ids
+    tracks = {}
+    chunk_size = 20
+    chunks = [album_ids[i:i+chunk_size] for i in range(0,len(album_ids),chunk_size)]
+    for chunk in chunks:
+        # album_data = MyMelody.CLIENT.album(album_id)
+        # print(album_data["name"] + " - " + "; ".join([x["name"] for x in album_data["artists"]]))
+        # tracks |= process_tracks([x["id"] for x in album_data["tracks"]["items"]], update=update)
+        
+        albums = MyMelody.CLIENT.albums(chunk)
+        for album in albums["albums"]:
+            print("  " + album["name"] + " - " + "; ".join([x["name"] for x in album["artists"]]))
+            album_sans_tracks = {k:v for k,v in album.items() if k not in ("tracks")}
+            for track in album["tracks"]["items"]:
+                track["album"] = album_sans_tracks
+                tracks[track["id"]] = set_track_metadata(track)
+                print("    " + get_track_description(track["id"]))
+    return tracks
 
 
 def process_artists(artist_ids):
-    track_ids = []
+    SORT_ORDER = {
+        "album": 0,
+        "single": 1,
+        "compilation": 2,
+        "appears_on": 3,
+    }
+    tracks = {}
     for artist_id in artist_ids:
-        artist_data = get_artist_metadata(artist_id)
-        print(artist_data["name"])
-        album_ids = []
-        offset = 0
-        limit = 50
+        artist_data = set_artist_metadata(MyMelody.CLIENT.artist(artist_id))
+        print("  " + artist_data["name"])
+
+        existing_tracks = {k:v for k,v in MyMelody.get_data("tracks").items() if artist_id in v["artists"]}
+
+        artist_albums = []
+        artist_tracks = []
+        sorted_tracks = []
+
+        # Get all albums containing track by artist
+        artist_albums_limit = 50
+        artist_albums_offset = 0
+        artist_albums_total = MyMelody.CLIENT.artist_albums(artist_id, limit=1)["total"]
+        artist_albums_progress = tqdm(total=artist_albums_total, desc="  Albums")
         while True:
-            args = {
-                "limit": limit,
-                "offset": offset,
-                "include_groups": "single,album"
-            }
-            albums = MyMelody.get_content_metadata("artist_albums", artist_id, args)
-            album_ids += [x["id"] for x in albums["items"]]
-            if len(albums["items"]) < limit:
+            artist_albums_resp = MyMelody.CLIENT.artist_albums(artist_id, limit=artist_albums_limit, offset=artist_albums_offset)["items"]
+            artist_albums_progress.update(len(artist_albums_resp))
+            artist_albums += artist_albums_resp
+            if len(artist_albums_resp) < artist_albums_limit:
                 break
-            offset += limit
-        track_ids += process_albums(album_ids)
-    return track_ids
+            artist_albums_offset += artist_albums_limit
+        artist_albums_progress.close()
+
+        # Get all tracks, from those albums, by artist
+        album_chunk_size = 20
+        album_ids = [x["id"] for x in artist_albums]
+        album_chunks = [album_ids[i:i+album_chunk_size] for i in range(0,len(album_ids),album_chunk_size)]
+        album_progress = tqdm(total=len(album_ids))
+        for chunk in album_chunks:
+            albums = MyMelody.CLIENT.albums(chunk)
+            for album in albums["albums"]:
+                album_progress.set_description("  "+album["name"])
+                album_sans_tracks = {k:v for k,v in album.items() if k not in ("tracks")}
+                tracks_by_artist = [x for x in album["tracks"]["items"] if artist_id in [y["id"] for y in x["artists"]]]
+                for track in tracks_by_artist:
+                    track["album"] = album_sans_tracks
+                    artist_tracks.append(track)
+                album_progress.update(1)
+        album_progress.close()
+
+        # Sorting tracks to remove duplicates from fluff albums
+        for track in artist_tracks:
+            if [x for x in existing_tracks.values() if x["name"] == track["name"]]:
+                continue
+            tracks_same_name = [x for x in sorted_tracks if x["name"] == track["name"]] + [track]
+            ordered_tracks = [i for i in sorted(tracks_same_name, key=lambda x: (artist_id in x["album"]["artists"], SORT_ORDER[x["album"]["album_type"]], x["album"]["release_date"]))]
+            sorted_tracks = [x for x in sorted_tracks if x not in ordered_tracks] + [ordered_tracks[0]]
+
+        print()
+        print("  Tracks:")
+        if not sorted_tracks:
+            print("    No new tracks")
+            continue
+        for track in sorted(sorted_tracks, key=lambda x: x["album"]["release_date"]):
+            tracks[track["id"]] = set_track_metadata(track)
+            print("    " + get_track_description(track["id"], album=True, artists=True))
+    return tracks
+
+
+def pull_artists():
+    for artist_id, artist_data in MyMelody.get_data("artists").items():
+        artist_tracks = process_artists([artist_id], update=False)
+        track_diff = [x for x in artist_tracks if x not in MyMelody.get_data("tracks")]
+        # track_diff = {k:v for k,v in MyMelody.get_data("tracks").items() if k not in artist_tracks and artist_id in v["album"]["artists"]}
+        if not track_diff:
+            continue
+        print(f"Missing {len(track_diff)} tracks from {artist_data['name']}:")
+        for track_id in track_diff:
+            print("  " + get_track_description(track_id, album=True, artists=False))
 
 
 ################################################################################
@@ -346,7 +456,7 @@ def process_artists(artist_ids):
 
 @click.group()
 def main():
-    pass
+    MyMelody()
 
 @main.command()
 @click.option(
@@ -374,31 +484,39 @@ def main():
     help="Only download the content passed",
 )
 def download(tracks, albums, artists, explicit):
-    MyMelody()
-    all_tracks = []
+    download_tracks = []
 
     if tracks:
         print("Processing tracks...")
-        all_tracks += process_tracks(tracks.split(","))
+        download_tracks += process_tracks(tracks.split(","))
         print()
     if albums:
         print("Processing albums...")
-        all_tracks += process_albums(albums.split(","))
+        download_tracks += process_albums(albums.split(","))
         print()
     if artists:
         print("Processing artists...")
-        all_tracks += process_artists(artists.split(","))
+        download_tracks += process_artists(artists.split(","))
         print()
 
     if not explicit:
-        return
+        download_tracks = tracks_to_download()
 
-    print("Downloading tracks")
-    download_tracks_safely(all_tracks)
-    print()
-    print("Cleaning up tracks and dirs")
     cleanup_tracks()
     MyMelody.write_data()
+
+    if download_tracks:
+        print(f"Downloading {len(download_tracks)} tracks:")
+        download_tracks_safely(download_tracks)
+
+@main.command()
+@click.argument("artist")
+def pull_artist(artist):
+    # process_artists([artist])
+    # MyMelody.write_data()
+    # MyMelody.CLIENT.search("")
+    # pull_artists()
+    pass
 
 if __name__ == "__main__":
     main()
